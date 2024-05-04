9662e3b8a60bcb8f8f68994872bc362df292ee72
Sonakshi Mishra <sonakshimishra29@gmail.com>
2024-05-04 16:08:36 +0530

    Changes to core components

--- jobsearchportal/src/App.js
+++ jobsearchportal/src/App.js
@@ -1,17 +1,31 @@
-import './App.css';
-import ConnectedFetchJobsComponent from './fetchjobsComponent';
-import { connectToRedux, ReduxProvider, updateFilter } from "./redux";
+import "./css/App.css";
+import ConnectedFetchJobsComponent from "./components/fetchJobsComponent";
+import { ReduxProvider } from "./components/redux";
 
 function App() {
   return (
     <div className="container">
       <ReduxProvider>
-      <h1 className="header">Job Portal</h1>
-      <div class="MuiBox-root css-jiivg1"><div class="MuiBox-root css-0"><p class="MuiTypography-root MuiTypography-body1 css-1i8efa4">👋 User</p></div><div class="MuiBox-root css-0"></div></div>
-      <div class="MuiBox-root css-1q44eny"><div class="MuiBox-root css-17qnvss"><p class="MuiTypography-root MuiTypography-body1 css-bkh8p3">Hi,Creating a user friendly job search portal, through which you can easily track jobs and their details. </p></div></div>
-      
-      <h2 className="subheading">Search Jobs</h2>
-      <ConnectedFetchJobsComponent/>
+        <h1 className="header">Job Portal</h1>
+        <div class="MuiBox-root css-jiivg1">
+          <div class="MuiBox-root css-0">
+            <p class="MuiTypography-root MuiTypography-body1 css-1i8efa4">
+              👋 User
+            </p>
+          </div>
+          <div class="MuiBox-root css-0"></div>
+        </div>
+        <div class="MuiBox-root css-1q44eny">
+          <div class="MuiBox-root css-17qnvss">
+            <p class="MuiTypography-root MuiTypography-body1 css-bkh8p3">
+              Hi, creating a user friendly job search portal, through which you
+              can easily track jobs and their details!{" "}
+            </p>
+          </div>
+        </div>
+
+        <h2 className="subheading">Search Jobs</h2>
+        <ConnectedFetchJobsComponent />
       </ReduxProvider>
     </div>
   );
